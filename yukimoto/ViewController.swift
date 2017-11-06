21ecc8ef549528f67e9f2e21f4f9c98479a08aa3
d.yukimoto <yukimoto@orangeinc.me>
2017-11-06 15:04:02 +0900

    Initial Commit

---
+++ yukimoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yukimoto
+//
+//  Created by 雪本大樹 on 2017/11/06.
+//  Copyright © 2017年 雪本大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
